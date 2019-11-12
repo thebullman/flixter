c00c6c642c7a8af36097fa9029bd13f4d91e5326
B. Kyrk Ullman <git@bkullman.com>
2019-11-12 01:42:05 +0000

    Lesson_28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_lesson, only: [:show]
+
   def show
   end
 
   private
+
+def require_enrolled_for_current_lesson
+  if !current_user.enrolled_in?(current_lesson.section.course)
+  redirect_to course_path(current_lesson.section.course), alert: 'You have to be enrolled to see the lesson!'
+  end
+end
 
   helper_method :current_lesson
   def current_lesson

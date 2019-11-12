362547bcb122224d273af88213493f541012aba6
B. Kyrk Ullman <git@bkullman.com>
2019-11-12 08:13:30 +0000

    Lesson_30:Ordering Lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :section
-  has_many :lessons
   mount_uploader :video, VideoUploader
+
+  include RankedModel
+  ranks :row_order, with_same: :section_id
 end

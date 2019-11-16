400271d50761c23c1374ace35d06ec2b9c5b2e58
B. Kyrk Ullman <git@bkullman.com>
2019-11-16 22:37:41 +0000

    Lesson 37

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,6 +5,11 @@ class Section < ApplicationRecord
   include RankedModel
   ranks :row_order, with_same: :course_id
 
-
   validates :title, presence: true
+
+  def next_section
+    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
+    return section
+  end
+
 end
